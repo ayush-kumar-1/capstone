0835609d4159cc4aa07fb06f56d9ce3661a548d4
ayush-kumar-1 <58648531+ayush-kumar-1@users.noreply.github.com>
2022-03-22 23:25:58 -0400

    Added custom stopwords, refactored code to not run test on import, added method to preprocess all tweets

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,20 +6,41 @@ Created on Tue Mar 22 20:55:09 2022
 @author: elisekarinshak
 """
 
-
 import pandas as pd
 import numpy as np
 import contractions
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
+nltk.download('omw-1.4')
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+
 nltk.download('wordnet')
 
-tweets = pd.read_csv("~/Desktop/STAT4990/Final Project/Provided information/cyberbullying_tweets.csv")
+
+def main(): 
+    tweets = pd.read_csv("~/Desktop/STAT4990/Final Project/Provided information/cyberbullying_tweets.csv")
+
+    #TEST----------------------------------------------------------------------
+
+    text_sample = '''    I cant go   to the gYm today, I'm tired lol :( gr8'''
+    out = to_lowercase(text_sample)  
+    out = expand_contractions(out)
+    out = remove_special(out)                                                                 
+    out_words = word_tokenize(out)
+    out_words = remove_stopwords(out_words)   
+    out_words = lemmatization(out_words)      
+    print(out_words)
+
+    
+    #APPLY TO DATASET ---------------------------------------------------------
+
+    for i in range(0, len(tweets["tweet_text"])):
+        tweets.iloc[i, 0] =  preprocess(tweets.iloc[i, 0])
 
 #before tokenization:
 #capitalization, contractions, symbols, white spaces
@@ -28,14 +49,14 @@ tweets = pd.read_csv("~/Desktop/STAT4990/Final Project/Provided information/cybe
 
 #PREPROCESSING METHODS
 #implementations of common NLP preprocessing methods
-                                                                                                         
+
 
 #METHODS BEFORE TOKENIZATION -----------------------------------------------
- 
+
 #Lower Case
 def to_lowercase(text):
     return text.lower()
-                                                              
+
 #Expand Contractions
 def expand_contractions(text):
     return contractions.fix(text)
@@ -54,6 +75,7 @@ def remove_special(text):
 #Remove Stopwords
 def remove_stopwords(text_arr):
     stops = set(stopwords.words('english'))
+    stops |= set(['rt', 'mkr', 'didn', 'bc', 'n', 'm', 'im', 'll', 'y', 've', 'u', 'ur', 'don', 't', 's']) #additional stopwords
     
     filtered = []
     for word in text_arr:
@@ -95,20 +117,8 @@ def preprocess(text):
     
     return out_words
 
+def preprocess_all(tweets):
+    return tweets.apply(preprocess)
 
-#TEST----------------------------------------------------------------------
-
-text_sample = '''    I cant go   to the gYm today, I'm tired lol :( gr8'''
-out = to_lowercase(text_sample)  
-out = expand_contractions(out)
-out = remove_special(out)                                                                 
-out_words = word_tokenize(out)
-out_words = remove_stopwords(out_words)   
-out_words = lemmatization(out_words)      
-print(out_words)
-
-
-#APPLY TO DATASET ---------------------------------------------------------
-
-for i in range(0, len(tweets["tweet_text"])):
-    tweets.iloc[i, 0] =  preprocess(tweets.iloc[i, 0])
+if __name__ == "__main__":
+    main()
